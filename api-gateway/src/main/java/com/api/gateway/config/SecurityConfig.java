9d385571979ff767598f205802f7852a002c0385
jonathan Diaz <jonatham01@hotmail.com>
2024-02-21 08:57:19 -0500

    habilitando las contraseñas

--- api-gateway/src/main/java/com/api/gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/api/gateway/config/SecurityConfig.java
@@ -12,18 +12,7 @@ import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import java.net.http.HttpClient;
-/*
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
-import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
-import org.springframework.security.web.server.SecurityWebFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
- */
 
 @EnableWebFluxSecurity
 public class SecurityConfig {
@@ -39,11 +28,7 @@ public class SecurityConfig {
                         .oauth2ResourceServer( oauth2 -> oauth2
                         .jwt(Customizer.withDefaults())
                 );
-                //.oauth2ResourceServer( oauth2 -> oauth2
-                       // .jwt(Customizer.withDefaults())
 
-
-              //  );
         return serverHttpSecurity.build();
     }
     @Bean
@@ -53,14 +38,7 @@ public class SecurityConfig {
 
 }
 
-/*
-    @Bean
-    public ReactiveJwtDecoder jwtDecoder() {
-        return NimbusReactiveJwtDecoder.withIssuerLocation("http://localhost:8081/realms/spring-boot-microservices-realm").build();
 
-    }
-
-*/
 
 /*
                         oauth2 -> oauth2
@@ -87,21 +65,4 @@ spring.main.allow-bean-definition-overriding=true
 server.ssl.enabled=false
 
 
- */
-
-/*return NimbusReactiveJwtDecoder.withJwkSetUri(jwkSetUri)
-
-                .jwsAlgorithm(SignatureAlgorithm.RS256).build();*/
-
-/*
-
-<dependency>
-			<groupId>org.springframework.boot</groupId>
-			<artifactId>spring-boot-starter-security</artifactId>
-		</dependency>
-
-		<dependency>
-			<groupId>org.springframework.boot</groupId>
-			<artifactId>spring-boot-starter-oauth2-resource-server</artifactId>
-		</dependency>
  */
---
+++ discovery-server/src/main/java/com/discovery/eureka/config/SecurityConfig.java
@@ -0,0 +1,64 @@
+package com.discovery.eureka.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Value("${app.eureka.username}")
+    private String username;
+
+    @Value("${app.eureka.password}")
+    private String password;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        UserDetails user = users.username(username)
+                .password(password)
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults())
+                .build();
+
+    }
+
+}
